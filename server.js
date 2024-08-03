7e6d92de70d196ae6c8e94c77085f8bab5fd0233
satyameazybe5554 <satyam@eazybe.com>
2024-08-03 15:26:45 +0530

    update

--- server.js
+++ server.js
@@ -1,51 +1,17 @@
-const http = require("http");
-const express = require("express");
-const path = require("path");
-const app = express();
-const PORT = 443;
-const server = http.createServer(app);
-const { Server } = require("socket.io");
-const cors = require('cors');
-const io = new Server(server);
-app.use(express.static(path.join(__dirname, "public")));
-app.use(cors({
-    origin: 'http://localhost:4000', // Replace with your client URL
-    methods: ['GET', 'POST']
-  }));
-
-io.on("connection", (socket) => {
-    console.log("A new user has connected", socket.id);
-    socket.on("message", (msg) => {
-        console.log(`Message from ${socket.id}: ${msg}`);
-        io.emit("message", msg);
-    });
-    socket.on("disconnect", () => {
-        console.log("User disconnected", socket.id);
-    });
-    socket.on("error", (error) => {
-        console.error("Socket.IO error:", error);
-    });
-});
-
-
-
-server.listen(PORT, () => {
-    console.log(`Server is running on ${PORT}`);
-});
-
-
-app.get('/', (req, res) => {
-    res.sendFile(path.join(__dirname, "public", "index.html"));
-});
-
-
 // const http = require("http");
 // const express = require("express");
+// const path = require("path");
 // const app = express();
-// const PORT = 4000;
+// const PORT = 443;
 // const server = http.createServer(app);
 // const { Server } = require("socket.io");
+// const cors = require('cors');
 // const io = new Server(server);
+// app.use(express.static(path.join(__dirname, "public")));
+// app.use(cors({
+//     origin: 'http://localhost:8080', // Replace with your client URL
+//     methods: ['GET', 'POST']
+//   }));
 
 // io.on("connection", (socket) => {
 //     console.log("A new user has connected", socket.id);
@@ -60,9 +26,49 @@ app.get('/', (req, res) => {
 //         console.error("Socket.IO error:", error);
 //     });
 // });
-// server.get('/',(req,res)=>{
-//     res.send("WS Running Successful");
-// })
+
+
+
 // server.listen(PORT, () => {
-//     console.log(`Server is running on port ${PORT}`);
+//     console.log(`Server is running on ${PORT}`);
 // });
+
+
+// app.get('/', (req, res) => {
+//     res.sendFile(path.join(__dirname, "public", "index.html"));
+// });
+
+
+const http = require("http");
+const express = require("express");
+const app = express();
+const PORT = 443;
+const server = http.createServer(app);
+const cors = require('cors');
+const io = new Server(server);
+app.use(express.static(path.join(__dirname, "public")));
+app.use(cors({
+    origin: 'http://localhost:8080', // Replace with your client URL
+    methods: ['GET', 'POST']
+  }));
+
+
+io.on("connection", (socket) => {
+    console.log("A new user has connected", socket.id);
+    socket.on("message", (msg) => {
+        console.log(`Message from ${socket.id}: ${msg}`);
+        io.emit("message", msg);
+    });
+    socket.on("disconnect", () => {
+        console.log("User disconnected", socket.id);
+    });
+    socket.on("error", (error) => {
+        console.error("Socket.IO error:", error);
+    });
+});
+server.get('/',(req,res)=>{
+    res.send("WS Running Successful");
+})
+server.listen(PORT, () => {
+    console.log(`Server is running on port ${PORT}`);
+});
