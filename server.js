e6d74b1e17063c95314358948af7d8cd97cefca4
satyameazybe5554 <satyam@eazybe.com>
2024-08-03 13:55:04 +0530

    update

--- server.js
+++ server.js
@@ -26,15 +26,11 @@
 
 const http = require("http");
 const express = require("express");
-const path = require("path");
 const app = express();
 const PORT = 4000;
 const server = http.createServer(app);
 const { Server } = require("socket.io");
 const io = new Server(server);
-
-
-app.use(express.static(path.join(__dirname, "public")));
 
 io.on("connection", (socket) => {
     console.log("A new user has connected", socket.id);
@@ -49,10 +45,9 @@ io.on("connection", (socket) => {
         console.error("Socket.IO error:", error);
     });
 });
-
-app.get('/', (req, res) => {
-    res.sendFile(path.join(__dirname, "public", "index.html"));
-});
+app.get('/',(req,res)=>{
+    console.log("WS Running Successful")
+})
 server.listen(PORT, () => {
     console.log(`Server is running on port ${PORT}`);
 });
